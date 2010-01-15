e541bd6d4f95ff32ae0b597acfd26eb07c013610
txithihausen <txithihausen@45d18298-010a-11df-b136-7f7962f7bc17>
2010-01-15 16:05:30 +0000

    This commit implements the encode and decode functions in SpeexPlugin. Also the Preprocess options were implemented
    TODO: implements preprocess step

--- trunk/plugins-stream/speexplugin.h
+++ trunk/plugins-stream/speexplugin.h
@@ -15,12 +15,36 @@
 #include <map>
 #include <speex/speex.h>
 #include <speex/speex_types.h>
+#include <speex/speex_preprocess.h>
 
 class SpeexPlugin : public PluginStreamIF {
+	enum SpeexMode {
+		MODE_NB,	// Narrow band
+		MODE_WB,	// Wide band
+		MODE_UWB	// Ultra wide band
+	};
+
+	struct HalfCodec {
+		SpeexMode mode;
+		SpeexBits bits;
+		void* state;
+	};
+
 	map<std::string, MessageType> constants;
 	void initConstants();
-	void* encoderPtr;
-	void* decoderPtr;
+	HalfCodec* encoder;
+	HalfCodec* decoder;
+	SpeexPreprocessState* preprocess;
+//	void* encoderPtr; //encoder
+//	void* decoderPtr; //decoder
+//	SpeexPreprocessState* preprocess; //preprocess
+//	SpeexBits encoderBits;
+//	SpeexBits decoderBits;
+	void buildEncoder(SpeexMode mode) throw(OperationNotPerfomedException);
+	void buildDecoder(SpeexMode mode) throw(OperationNotPerfomedException);
+
+	void destroyEncoder() throw(OperationNotPerfomedException);
+	void destroyDecoder() throw(OperationNotPerfomedException);
 public:
 	SpeexPlugin();
 	virtual ~SpeexPlugin();
@@ -30,7 +54,14 @@ public:
 
 	virtual void adaptStream(std::string paramName, std::map<std::string,
 			std::string> &params)
-				throw (OperationNotPerfomedException);
+				throw (OperationNotPerfomedException, OperationNotSupportedException);
+
+
+	virtual uint16 encode(int16 *sample_buf, uint16 nsamples, uint8 *payload,
+			uint16 payload_size, bool &silence) throw(OperationNotPerfomedException);
+
+	virtual uint16 decode(uint8 *payload, uint16 payload_size, int16 *pcm_buf,
+			uint16 pcm_buf_size) throw(OperationNotPerfomedException);
 
 	virtual const char* getName() const;
 };
